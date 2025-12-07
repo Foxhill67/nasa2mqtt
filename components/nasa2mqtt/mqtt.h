be08227053e6f973eccd8a78f0baeb657fee72d6
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-12-07 21:21:15 +0100

    Update mqtt.h

--- components/nasa2mqtt/mqtt.h
+++ components/nasa2mqtt/mqtt.h
@@ -8,5 +8,8 @@ namespace esphome
         bool mqtt_connected();
         void mqtt_connect(const std::string &host, const uint16_t port, const std::string &username, const std::string &password);
         bool mqtt_publish(const std::string &topic, const std::string &payload);
+#ifdef USE_ESP32
+        extern volatile bool is_mqtt_connected;
+#endif
     } // namespace nasa2mqtt
 } // namespace esphome
